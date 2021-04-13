76d8377e92d3d808fe0a785d641b7dcf567c9389
Vaadeendra <42719383+Vaadee@users.noreply.github.com>
2021-04-13 10:12:20 +0530

    Comments to the code have been added

--- seleniumAssignment.py
+++ seleniumAssignment.py
@@ -1,11 +1,9 @@
-import time, requests
+# Importing required libraries
+import time, requests, progressbar, csv, pandas as pd
 from selenium import webdriver
-import pandas as pd
-import numpy as np
-import progressbar
-import csv
 from selenium.webdriver.chrome.options import Options
 
+# Initializing the progressbar
 widgets = [
     " [",
     progressbar.Timer(format="elapsed time: %(elapsed)s"),
@@ -16,9 +14,9 @@ widgets = [
     ") ",
 ]
 
+# Initializing the required lists
 masterLoadData = []
 masterSiteData = []
-
 websites = [
     "https://nrega.nic.in/netnrega/home.aspx",
     "https://www.usa.gov/",
@@ -28,18 +26,20 @@ websites = [
     # "https://cms.bits-hyderabad.ac.in/login/index.php",
     # "https://swd.bits-hyderabad.ac.in/",
 ]
+timeCalc = maxTime = 0
+minTime = 99999999
 
+# Initializing selenium webDriver
 options = Options()
 options.add_argument("ignore-certificate-errors")
 options.add_argument("--allow-running-insecure-content")
 
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
-timeCalc = maxTime = 0
-minTime = 99999999
-
+# Loop that runs the code on all the websites provided in the list above
 for site in websites:
+    # Initializing temperary variables for the loop
     driver.get(site)
     listOfWebsites = []
     websiteCount = []
@@ -48,6 +48,8 @@ for site in websites:
     timeCalc = 0
     countValid = countInvalid = 0
     links = driver.find_elements_by_css_selector("a")
+
+    # Checking if the list of links is less than 100. (Helpful for websites with lazy scroll)
     while len(links) < 100:
         try:
             driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
@@ -55,6 +57,8 @@ for site in websites:
             time.sleep(0.5)
         except:
             break
+
+    # Capturing unique set of links from the main website. This helps reduce the redundency in testing the same link multiple times.
     for testLink in links:
         if testLink.get_attribute("href") not in listOfWebsites:
             listOfWebsites.append(testLink.get_attribute("href"))
@@ -62,15 +66,20 @@ for site in websites:
         else:
             index = listOfWebsites.index(testLink.get_attribute("href"))
             websiteCount[index] += 1
+
+    # Prints the current state of the website to the console.
     print(
         "Currently testing site {}, the total number of links is {} so this could take a while.".format(
             site, len(links)
         )
     )
 
+    # Calling the progressbar
     bar = progressbar.ProgressBar(
         max_value=len(listOfWebsites), widgets=widgets
     ).start()
+
+    # Loop that works with each sublink in the main website
     for link in listOfWebsites:
         index = listOfWebsites.index(link)
         try:
@@ -101,19 +110,23 @@ for site in websites:
                 countInvalid += 1 * websiteCount[index]
                 status = "Y"
             masterLoadData.append([site, link, "ACT-Fibernet", tempTime / 5, status])
-            # print("[", site, link, "ACT-Fibernet", tempTime / 5, status, "]")
             timeCalc += (tempTime / 5) * websiteCount[index]
         except:
             countInvalid += 1 * websiteCount[index]
             status = "Y"
             masterLoadData.append([site, "NA", "ACT-Fibernet", "NA", status])
-            # print("[", site, link, "ACT-Fibernet", "NA", status, "]")
+
+        # Updates the progressbar after some interval of time
         bar.update(listOfWebsites.index(link))
+
+    # Prints the current state of the program to console
     print(
         "\nComplete.\nThe count of valid links in {} is {} and invalid links is {}".format(
             site, countValid, countInvalid
         )
     )
+
+    # Final calculations before the array is converted DataFrame
     averageLinkLoadTime = timeCalc / countValid
     if averageLinkLoadTime < minTime:
         minTime = averageLinkLoadTime
@@ -130,12 +143,15 @@ for site in websites:
     )
 
 driver.quit()
+
+# Calculates the Website Score
 print(maxTime, minTime)
 for row in masterSiteData:
     A = (float(row[2]) - minTime) / (maxTime - minTime)
     B = float(row[3]) / (float(row[3]) + float(row[4]))
     row.append((A + B) / 2)
 
+# Transforms the array to FataFrame. Makes it easier to save to a CSV file
 masterLoadData = pd.DataFrame(
     masterLoadData,
     columns=[
@@ -157,7 +173,11 @@ masterSiteData = pd.DataFrame(
         "Website Score",
     ],
 )
+
+# Sorts the siteData by the Website Score
 masterSiteData = masterSiteData.sort_values(by=["Website Score"])
+
+# Saves to CSV
 masterLoadData.to_csv(
     "loadData.csv",
     index=False,
