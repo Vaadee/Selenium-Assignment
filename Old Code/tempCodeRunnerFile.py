a1e23748a4d63403ad1bd9c6fec8c399460a19c5
Vaadeendra <42719383+Vaadee@users.noreply.github.com>
2021-05-02 14:24:25 +0530

    Old Code has been deleted

--- Old Code/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
- "Website",
-        "Broadnand Provider",
-        "Average Link Load Time",
-        "Number of Dead Links/ Time Outs",
-        "Number of Working Links",
-        "Website Score",
-    ],
-)
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-"https://www.usa.gov/",
-    # "https://www.isro.gov.in/",
-    # "https://www.bits-pilani.ac.in/",
-    # "https://medium.com/",
